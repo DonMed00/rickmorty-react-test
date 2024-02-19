595d2335e0ff7f47402b1e19e19df41ab3a228b2
Adrian <xmena97@gmail.com>
2024-02-19 18:29:52 +0100

    fixes

--- App.js
+++ App.js
@@ -4,15 +4,17 @@ import { createNativeStackNavigator } from '@react-navigation/native-stack'
 import Home from './src/components/Home';
 import Login from './src/components/Login';
 
-const Stack = createNativeStackNavigator()
+const Stack = createNativeStackNavigator();
 
-export default function App() {
+function App() {
   return (
     <NavigationContainer>
-       <Stack.Navigator initialRouteName='Login'>
-      <Stack.Screen name="Loagin" component={Login} />
-      <Stack.Screen name="Home" component={Home} />
-    </Stack.Navigator>
+     <Stack.Navigator initialRouteName='Login'>
+      <Stack.Screen name="Login" component={Login} />
+        <Stack.Screen name="Home" component={Home} />
+      </Stack.Navigator>
     </NavigationContainer>
   );
 }
+
+export default App;
