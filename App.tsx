be4955f828a5273cadb3886f0839dcd37ce4903b
Adrian <xmena97@gmail.com>
2024-02-26 17:53:03 +0100

    added eslint

--- App.tsx
+++ App.tsx
@@ -1,20 +1,20 @@
 import React from 'react'
 import { NavigationContainer } from '@react-navigation/native'
 import { createNativeStackNavigator } from '@react-navigation/native-stack'
-import Home from './src/screens/HomeScreen/Home';
-import Login from './src/screens/LoginScreen/Login';
+import Home from './src/screens/HomeScreen/Home'
+import Login from './src/screens/LoginScreen/Login'
 
-const Stack = createNativeStackNavigator();
+const Stack = createNativeStackNavigator()
 
 function App() {
   return (
     <NavigationContainer>
-     <Stack.Navigator initialRouteName='Login'>
-      <Stack.Screen name="Login" component={Login} />
+      <Stack.Navigator initialRouteName="Login">
+        <Stack.Screen name="Login" component={Login} />
         <Stack.Screen name="Home" component={Home} />
       </Stack.Navigator>
     </NavigationContainer>
-  );
+  )
 }
 
-export default App;
+export default App
--- src/screens/LoginScreen/Login.tsx
+++ src/screens/LoginScreen/Login.tsx
@@ -5,12 +5,9 @@ const Login = ({ navigation }) => {
   return (
     <View>
       <Text>Login</Text>
-      <Button
-        title="Go to Home"
-        onPress={() => navigation.navigate('Home')}
-      />
+      <Button title="Go to Home" onPress={() => navigation.navigate('Home')} />
     </View>
-  );
-};
+  )
+}
 
 export default Login
